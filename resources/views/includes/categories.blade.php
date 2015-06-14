317c003da2f10a86095acfc9f084c008b1f6daba
Gerhard Botha <gerhardtheunissen10@gmail.com>
2015-06-14 21:22:18 +0200

    Update

---
+++ resources/views/includes/categories.blade.php
@@ -0,0 +1,22 @@
+@inject('categories', 'App\Categories\Category')
+
+<div class="widget-categories  push-down-30">
+	<h6>Categories</h6>
+	<ul>
+		<li>
+			<a href="#">Laravel &nbsp; <span class="widget-categories__text">(16)</span> </a>
+		</li>
+		<li>
+			<a href="#">PHP &nbsp; <span class="widget-categories__text">(13)</span> </a>
+		</li>
+		<li>
+			<a href="#">Javascript &nbsp; <span class="widget-categories__text">(9)</span> </a>
+		</li>
+		<li>
+			<a href="#">VueJs &nbsp; <span class="widget-categories__text">(23)</span> </a>
+		</li>
+		<li>
+			<a href="#">Envoyer &amp; Books &nbsp; <span class="widget-categories__text">(3)</span> </a>
+		</li>
+	</ul>
+</div>
--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,140 +1,25 @@
 <div class="col-xs-12  col-md-4">
-		<!-- Widget author -->
-		<div class="widget-author  boxed  push-down-30">
-			<div class="widget-author__image-container">
-				<div class="widget-author__avatar--blurred">
-					<img src="/images/gerhard.jpg" alt="Gerhard Botha" width="90" height="90">
-				</div>
-				<img class="widget-author__avatar" src="/images/gerhard.jpg" alt="Gerhard Botha" width="90" height="90">
+	<div class="widget-author  boxed  push-down-30">
+		<div class="widget-author__image-container">
+			<div class="widget-author__avatar--blurred">
+				<img src="/images/gerhard.jpg" alt="Gerhard Botha" width="90" height="90">
 			</div>
-			<div class="row">
-				<div class="col-xs-10  col-xs-offset-1">
-					<h4>Gerhard Botha</h4>
-					<p>I’m a self-proclaimed perfectionist with a passion for all things web, which is exceeded only by my obsession with spicy food.</p>
-				</div>
-			</div>
+			<img class="widget-author__avatar" src="/images/gerhard.jpg" alt="Gerhard Botha" width="90" height="90">
 		</div>
-		<div class="sidebar  boxed  push-down-30">
-			<div class="row">
-				<div class="col-xs-10  col-xs-offset-1">
-					<!-- Widget categories -->
-					<div class="widget-categories  push-down-30">
-						<h6>Categories</h6>
-						<ul>
-							<li>
-								<a href="#">Laravel &nbsp; <span class="widget-categories__text">(16)</span> </a>
-							</li>
-							<li>
-								<a href="#">PHP &nbsp; <span class="widget-categories__text">(13)</span> </a>
-							</li>
-							<li>
-								<a href="#">Javascript &nbsp; <span class="widget-categories__text">(9)</span> </a>
-							</li>
-							<li>
-								<a href="#">VueJs &nbsp; <span class="widget-categories__text">(23)</span> </a>
-							</li>
-							<li>
-								<a href="#">Envoyer &amp; Books &nbsp; <span class="widget-categories__text">(3)</span> </a>
-							</li>
-						</ul>
-					</div>
-					<!-- Widget featured post -->
-					<div class="widget-featured-post  push-down-30">
-						<h6>Featured post</h6>
-						<img src="/images/dummy-licensed/293x127xblog-image.jpg.pagespeed.ic.26kd1AnH3b.jpg" alt="Featured post" width="293" height="127">
-						<h4>
-						<a href="single-post.html">Featured Author Post</a>
-						</h4>
-						<p>Why ProteusThemes products is so much better than lorem everyone else's.</p>
-					</div>
-					<!-- Widget posts -->
-					<div class="widget-posts  push-down-30">
-						<h6>Popular / Recent</h6>
-						<!-- Nav tabs -->
-						<ul class="nav  nav-tabs">
-							<li class="active">
-								<a href="#recent-posts" data-toggle="tab"> <span class="fa  fa-time"></span> &nbsp;Recent Posts </a>
-							</li>
-							<li> <a href="#popular-posts" data-toggle="tab"> <span class="fa  fa-flash"></span> &nbsp;Popular Posts </a> </li>
-						</ul>
-						<!-- Tab panes -->
-						<div class="tab-content">
-							<div class="tab-pane  fade  in  active" id="recent-posts">
-								<div class="push-down-15">
-									<img src="/images/dummy-licensed/xblog-image-small.jpg.pagespeed.ic.xUKfO4lWHu.jpg" alt="Posts" width="90" height="60">
-									<h5>
-									<a href="single-post.html">This is a showcase of the most recent posts</a>
-									</h5>
-									<span class="widget-posts__time">19 hours ago</span>
-								</div>
-								<div class="push-down-15">
-									<img src="/images/dummy-licensed/xblog-image-3-small.jpg.pagespeed.ic.K2378TKtZN.jpg" alt="Posts" width="90" height="60">
-									<h5>
-									<a href="single-post.html">This is a showcase of the most recent posts</a>
-									</h5>
-									<span class="widget-posts__time">19 hours ago</span>
-								</div>
-								<div class="push-down-15">
-									<img src="/images/dummy-licensed/xblog-image-small.jpg.pagespeed.ic.xUKfO4lWHu.jpg" alt="Posts" width="90" height="60">
-									<h5>
-									<a href="single-post.html">This is a showcase of the most recent posts</a>
-									</h5>
-									<span class="widget-posts__time">19 hours ago</span>
-								</div>
-							</div>
-							<div class="tab-pane  fade" id="popular-posts">
-								<div class="push-down-15">
-									<img src="/images/dummy-licensed/xblog-image-3-small.jpg.pagespeed.ic.K2378TKtZN.jpg" alt="Posts" width="90" height="60">
-									<h5>
-									<a href="single-post.html">This is a showcase of the most popular posts</a>
-									</h5>
-									<span class="widget-posts__time">9 hours ago</span>
-								</div>
-								<div class="push-down-15">
-									<img src="/images/dummy-licensed/xblog-image-small.jpg.pagespeed.ic.xUKfO4lWHu.jpg" alt="Posts" width="90" height="60">
-									<h5>
-									<a href="single-post.html">This is a showcase of the most popular posts</a>
-									</h5>
-									<span class="widget-posts__time">12 hours ago</span>
-								</div>
-								<div class="push-down-15">
-									<img src="/images/dummy-licensed/xblog-image-3-small.jpg.pagespeed.ic.K2378TKtZN.jpg" alt="Posts" width="90" height="60">
-									<h5>
-									<a href="single-post.html">This is a showcase of the most popular posts</a>
-									</h5>
-									<span class="widget-posts__time">19 hours ago</span>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="tags  widget-tags">
-						<h6>Tags</h6>
-						<hr>
-						<a href="#" class="tags__link">Tech</a>
-						<a href="#" class="tags__link">Web</a>
-						<a href="#" class="tags__link">UI/UX</a>
-						<a href="#" class="tags__link">Tutorials</a>
-						<a href="#" class="tags__link">Workflow</a>
-					</div>
-					<div class="widget-recent-comments  push-down-30">
-						<h6>Recent Comments</h6>
-						<ul>
-							<li>
-								Mihael on
-								<a href="single-post.html#comments">ProteusThemes and its products are great</a>
-								<br/>
-							</li>
-							<li>
-								Jaka on
-								<a href="single-post.html#comments">Readable is most readable WordPress theme out there</a>
-							</li>
-							<li>
-								Mihael on
-								<a href="single-post.html#comments">Everybody loves Readable</a>
-							</li>
-						</ul>
-					</div>					
-				</div>
+		<div class="row">
+			<div class="col-xs-10  col-xs-offset-1">
+				<h4>Gerhard Botha</h4>
+				<p>I’m a self-proclaimed perfectionist with a passion for all things web, which is exceeded only by my obsession with spicy food.</p>
 			</div>
 		</div>
 	</div>
+	<div class="sidebar  boxed  push-down-30">
+		<div class="row">
+			<div class="col-xs-10  col-xs-offset-1">
+				@include('includes.categories')
+				@include('includes.recent-posts')
+				@include('includes.tags')
+			</div>
+		</div>
+	</div>
+</div>
